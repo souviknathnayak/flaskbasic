36f5b119e3df1ef9a4dd452659d2e38a6c66babc
Souvik Nayak <souvik.nayak@live.com>
2025-01-04 12:12:51 +0530

    Pushing Buttons

--- app.py
+++ app.py
@@ -5,18 +5,23 @@ It creates an instance of the Flask class,
 which will be your WSGI (Web Server Gateway Interface) application.
 
 '''
-#test
 
 ##WSGI Application
 app= Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "<html><h1>Welcome to this best Flask course. This should be an amazing course</h1></html>"
+    return render_template("home.html")
 
 @app.route("/index")
 def index():
     return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
